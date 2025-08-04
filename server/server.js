4a7848a60081297581a5b25ef43a108b9d2677ca
Simthass Mohammed <135488850+Simthass@users.noreply.github.com>
2025-08-04 18:44:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,7 @@
-// server/server.js
-import express from 'express';
-import dotenv from 'dotenv';
+
 import contactRoute from './routes/contact.js'; // ✅ Use full path with .js
-
-dotenv.config();
-
-const app = express();
 
 app.use(express.json()); // Parse incoming JSON
 
 // Mount contact route
 app.use('/api/contact', contactRoute);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}`);
-});
